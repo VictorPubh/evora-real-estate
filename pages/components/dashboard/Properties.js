dd2b5ef736f3d1ad3ab6eb0a191e36af683bfac1
João Victor <viitorsant@gmail.com>
2021-05-18 02:23:07 -0300

    feature/ Modal

--- pages/components/dashboard/Properties.js
+++ pages/components/dashboard/Properties.js
@@ -1,16 +1,20 @@
-import { Form, Button } from "react-bootstrap"
+import { Form, Button, Modal } from "react-bootstrap"
 import { useState } from 'react'
 import axios from 'axios'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faMinus, faPlus, faSave } from '@fortawesome/free-solid-svg-icons'
+import { faMinus, faPlus, faSave, faTrash } from '@fortawesome/free-solid-svg-icons'
 import 'basscss'
 
-function Properties() {
+function Properties(props) {
     const [formTitle, setFormTitle] = useState()
     const [formDetails, setFormDetails] = useState()
     const [formSale, setFormSale] = useState(false)
     const [listImages, setListImages] = useState([])
+
+    // Modal
+    const [show, setShow] = useState(false)
+    const switchShow = () => setShow(!show)
 
     const mainImageInput = [<Form.Control className="mb1" onChange={(e) => setValueInputList(e.target.value, -1)} key="main" />]
     const newImageInput = (i) => <Form.Control className="mb1" key={i} onChange={(e) => setValueInputList(e.target.value, i)} />
@@ -54,7 +58,15 @@ function Properties() {
         console.log(req)
     }
 
+    let deleteButton
+    if(props.ID !== "new") {
+        deleteButton = <Button variant="danger" className="right mr1">
+                        <FontAwesomeIcon icon={faTrash} onClick={switchShow}/> Deletar
+                       </Button> 
+    }
+
     return (
+        <>
         <Form style={styles.container} onSubmit={submitForm} className="p2">
             <Form.Group>
                 <Form.Label>Título</Form.Label>
@@ -71,16 +83,33 @@ function Properties() {
                 <Form.Label>Imagens do Carousel</Form.Label>
                 { imgInputs }
                 <Button variant="success" className="mr1" onClick={addFormInput}>
-                    <FontAwesomeIcon icon={faPlus} /> Imagem
+                    <FontAwesomeIcon icon={faPlus} />
                 </Button>
                 <Button variant="danger" className="mr1" onClick={subtractFormInput}>
-                    <FontAwesomeIcon icon={faMinus} /> Imagem
+                    <FontAwesomeIcon icon={faMinus} />
                 </Button>
                 <Button type="submit" className="right">
                     <FontAwesomeIcon icon={faSave} /> Salvar Imóvel
                 </Button> 
+                { deleteButton }
             </Form.Group>
         </Form>
+
+        <Modal show={show} onHide={switchShow}>
+            <Modal.Header closeButton>
+            <Modal.Title>Você tem certeza?</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Se você clicar em "Sim, excluir" não poderá voltar atrás.</Modal.Body>
+            <Modal.Footer>
+            <Button variant="secondary" onClick={switchShow}>
+                Não, descartar
+            </Button>
+            <Button variant="danger" onClick={switchShow}>
+                Sim, excluir
+            </Button>
+            </Modal.Footer>
+        </Modal>
+        </>
     )
 }
 
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,23 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import { Tabs, Tab } from 'react-bootstrap'
 import Header from './components/dashboard/Header'
 import Properties from './components/dashboard/Properties'
 import Listing from './components/Listing'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'basscss'
+import axios from 'axios'
 
 function Dashboard() {
+    const router = useRouter()
+    const query = router.query || null
+    const [key, setKey] = useState(1)
+
+    useEffect(() => {
+        console.log(query)
+        if (query) {
+            setKey(3)
+        }
+    }, [])
+    
     return (
         <>
             <Header />
-            <Tabs>
-                <Tab eventKey="new" title="Criar Imóvel">
-                    <Properties />
+            <Tabs activeKey={key} onSelect={(k) => setKey(k)}>
+                <Tab eventKey={1} title="Criar Imóvel">
+                    <Properties ID="new1" />
                 </Tab>
-                <Tab eventKey="properties" title="Imóveis">
+                <Tab eventKey={2} title="Imóveis">
                     <Listing />
                 </Tab>
-                <Tab eventKey="editProperties" title="Editar Imóveis" disabled>
-                    
+                <Tab eventKey={3} title="Editar Imóveis" disabled>
+                    <Properties ID="new2" />
                 </Tab>
             </Tabs>
         </>
